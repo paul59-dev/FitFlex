0ac8d11a1b184893d94b5b2ef910ce378007ac07
Paul-developpeur <paulallebee@gmail.com>
2024-01-10 19:32:45 +0100

    feat(db): connect to mongodb

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
-require('dotenv').config();
-
+require('dotenv').config({path: './config/.env'});
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+// connect to the BD
+connectDB();
 
 app.use(cors());
 
