b43760cabf91e8574d2382eda2063742b8c47402
Paul-developpeur <paulallebee@gmail.com>
2024-01-10 22:50:20 +0100

    add sass

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 
 // connect to the BD
 connectDB();
